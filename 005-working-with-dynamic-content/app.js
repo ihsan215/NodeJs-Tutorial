bfbab512adb1f05365b536508eeeea7b6d89fd07
ihsan215 <aliihsantas34@gmail.com>
2023-10-25 10:46:55 +0300

    added handlebars

--- 005-working-with-dynamic-content/app.js
+++ 005-working-with-dynamic-content/app.js
@@ -2,10 +2,19 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.set("view engine", "pug");
+app.engine(
+  "hbs",
+  expressHbs({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
@@ -19,7 +28,7 @@ app.use("/admin", adminData.routers);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- 005-working-with-dynamic-content/routes/shop.js
+++ 005-working-with-dynamic-content/routes/shop.js
@@ -11,9 +11,12 @@ router.get("/", (req, res, next) => {
   const products = adminData.products;
   res.render("shop", {
     prods: products,
-    docTitle: "Shop",
+
     path: "/",
     pageTitle: "Shop",
+    hadProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true,
   });
 });
 
