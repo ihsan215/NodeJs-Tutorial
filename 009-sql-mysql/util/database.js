9b13ff9992bf4a139343327d65af9593f095742a
ihsan215 <aliihsantas34@gmail.com>
2023-10-27 13:10:17 +0300

    added lesson sql

---
+++ 009-sql-mysql/util/database.js
@@ -0,0 +1,11 @@
+require("dotenv").config({ path: ".env" });
+const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  database: "node-complete",
+  password: process.env.PASSWORD,
+});
+
+module.exports = pool.promise();
